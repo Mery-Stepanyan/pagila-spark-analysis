67af7ca066b526e78eecfb811594adf5a00bdfd8
Meri <stepanyan.mery@yahoo.com>
2026-04-20 01:04:39 +0400

    [PGL-001]: clean main branch - move code to develop for review

--- config.py
+++
@@ -1,9 +0,0 @@
-JAR_PATH = "file:///C:/spark_driver/postgresql-42.7.10.jar"
-
-
-DB_URL = "jdbc:postgresql://localhost:5432/pagila"
-DB_PROPERTIES = {
-    "user": "postgres",
-    "password": "YOUR_PASSWORD", # Fill in your password
-    "driver": "org.postgresql.Driver"
-}
